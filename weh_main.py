f9b04167dd3f6d35ee576992afed05e465a1b9d4
KenB98 <k.ortiz@ufl.edu>
2024-07-23 12:24:00 -0400

    KO_decoded_file

--- weh_main.py
+++ weh_main.py
@@ -5,3 +5,24 @@ def encode(password):
         new_num = (num + 3) % 10
         encoded += str(new_num)
     return encoded
+
+def decode(password):
+    new = []
+    for char in password:
+        number = int(char)
+        new.append(number)
+
+    decoded = ""
+    for val in new:
+        if val > 2:
+            val -= 3
+            decoded += str(val)
+        else:
+            if val == 2:
+                val = "9"
+            elif val == 1:
+                val = "8"
+            elif val == 0:
+                val = "7"
+            decoded += str(val)
+    return "".join(decoded)
